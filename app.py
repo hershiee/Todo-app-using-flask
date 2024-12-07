3fdf52a95cd06cd3c0de73c00e878a5e915e10ce
Harshita Gupta <beinginsatiable@gmail.com>
2024-12-07 10:41:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class todo(db.Model):
         return f"{self.sno} - {self.title}"
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_world():
+def title():
     if request.method == "POST":
         title = request.form['title']
         desc = request.form['desc']
